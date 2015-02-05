dd37897b892dafc2ad82cefdb287282fa44f3303
Jinguancai <jinw@uchicago.edu>
2015-02-04 21:17:30 -0600

    this is assignment 4

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[ ]:
+
 import csv
 import sys
 
@@ -13,4 +18,39 @@ def readCSV(filename):
     return(lines)
 
 
+
+
 ### enter your code below
+def latlong ():
+    n = len(readCSV("permits_hydepark.csv"))
+    k = readCSV("permits_hydepark.csv")
+    sum_latitude = 0
+    sum_longtitude = 0
+    for i in k:
+    	sum_latitude += float(i[-3])
+    	sum_longtitude += float(i[-2])
+    avg_latitude = float(sum_latitude/n)
+    avg_longitude = float(sum_longtitude/n)
+    print avg_latitude,avg_longitude
+
+def hist():
+    import matplotlib.pyplot as plt
+    import numpy as np 
+    n = len(readCSV("permits_hydepark.csv"))
+    k = readCSV("permits_hydepark.csv")
+    zipcode = []
+    z = [28,35,42,49,56,63,70]
+    for i in k:
+        for t in z:
+            if i[t] != '':
+                j = int(i[t][:5])
+                zipcode.append(j)    
+        
+    #print zipcode    
+    plt.hist(zipcode, bins=50)
+    plt.savefig("histogram.jpg")
+
+if sys.argv[1] == "latlong":
+    latlong()
+elif sys.argv[1] ==  "hist":
+    hist()
---
+++ assignment4/script.sh
@@ -0,0 +1,2 @@
+wc -l permits.csv
+grep "Hyde\sPark" permits.csv > permits_hydepark.csv
